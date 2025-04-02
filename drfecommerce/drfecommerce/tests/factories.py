3857b073828e7c05b803ec3f2546fa21adb1dd8c
Prince Ugochukwu Chime <48451931+Hy-light@users.noreply.github.com>
2025-04-02 16:29:10 +0100

    Developed the productline features and all test (#2)

--- drfecommerce/drfecommerce/tests/factories.py
+++ drfecommerce/drfecommerce/tests/factories.py
@@ -3,40 +3,60 @@ Factory is used to create dummy database objects needed for testing.
 """
 
 import factory
+
 # from factory import Faker, LazyFunction
 
-from drfecommerce.product.models import Category, Brand, Product
+from drfecommerce.product.models import Category, Brand, Product, ProductLine
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     """
     Factory for creating Category instances.
     """
+
     class Meta:
         model = Category
-        
+
     name = factory.Sequence(lambda n: "Category_%d" % n)
-  
-    
+
+
 class BrandFactory(factory.django.DjangoModelFactory):
     """
     Factory for creating Brand instances.
     """
+
     class Meta:
         model = Brand
-        
+
     name = factory.Sequence(lambda n: "Brand_%d" % n)
-    
+
 
 class ProductFactory(factory.django.DjangoModelFactory):
     """
     Factory for creating Product instances.
     """
+
     class Meta:
         model = Product
-        
+
     name = "test_product"
     description = "test_description"
     is_digital = True
     category = factory.SubFactory(CategoryFactory)
     brand = factory.SubFactory(BrandFactory)
+    is_active = True
+
+
+class ProductLineFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for creating ProductLine instances.
+    """
+
+    class Meta:
+        model = ProductLine
+
+    product = factory.SubFactory(ProductFactory)
+    sku = "1234"
+    stock_qty = 100
+    price = 10.00
+    is_active = True
